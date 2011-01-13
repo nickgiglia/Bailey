9bb81dd649d378f504950f830bbeb0a2abc19346
U-NickGiglia\Nick <Nick@NickGiglia.(none)>
2011-01-13 01:22:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bailey::Application.config.secret_token = '1b444004f28b00847073467dfffbd5a0096985fc713f53e7b299ed2ca29d3568961a96abef3624cda61bc7136473c7585c6f709c83957f0b7515a0c8390ef851'
